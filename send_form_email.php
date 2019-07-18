89be044cae5034df5ed8a58cb3a1bbe65b73e5cb
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2019-07-18 15:41:51 -0500

    update web

--- send_form_email.php
+++ send_form_email.php
@@ -1,8 +1,8 @@
 <?php
-if(isset($_POST['email'])) {
+if(isset($_POST['submit'])) {
  
-    $email_to = "kelly.urango@carvajal.com";
-    $email_subject = "algooo";
+    $email_to = "estiven.landazury@carvajal.com";
+    $email_subject = "Solicitud de importancia";
  
     function died($error) {
         // your error code can go here
@@ -16,8 +16,7 @@ if(isset($_POST['email'])) {
  
     // validation expected data exists
     if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
+        !isset($_POST['enterprise']) ||
         !isset($_POST['telephone']) ||
         !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
@@ -26,17 +25,17 @@ if(isset($_POST['email'])) {
      
  
     $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
+    $enterprise = $_POST['enterprise']; // required
+    $telephone = $_POST['telephone']; // required
     $comments = $_POST['comments']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
+ /*
   if(!preg_match($email_exp,$email_from)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
+  */
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
@@ -44,7 +43,7 @@ if(isset($_POST['email'])) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
+  if(!preg_match($string_exp,$enterprise)) {
     $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
  
@@ -67,8 +66,8 @@ if(isset($_POST['email'])) {
      
  
     $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Enterprise: ".clean_string($enterprise)."\n";
+   /* $email_message .= "Email: ".clean_string($email_from)."\n";*/
     $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
@@ -76,7 +75,12 @@ if(isset($_POST['email'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
+$enviado= @mail($email_to, $email_subject, $email_message, $headers); 
+
+if ($enviado)
+  echo 'Email enviado correctamente';
+else
+  echo 'Error en el envío del email';
 ?>
  
 <!-- include your own success html here -->
